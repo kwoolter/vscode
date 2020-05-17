09cd7bf491e46ba9405296affe7613c14d5039c6
kwoolter <keith.woolterton@gmail.com>
2020-05-17 17:10:30 +0800

    added plotting

--- project1/main.py
+++ project1/main.py
@@ -1,4 +1,7 @@
 import package1 as p1
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
 
 def main():
     print("Hello World")
@@ -6,10 +9,23 @@ def main():
     p1.f1()
     p1.f2()
 
-    a = p1.A()
-    b = p1.B()
+    df = pd.DataFrame([], columns=list("XYZ"))
 
-    print(a,b)
+    a = p1.A("Keith")
+    
+    for i in range(0,10):
+        #a.move(p1.A.VECTOR_NORTH, 2)
+        a.move(random.choice(p1.A.VALID_VECTORS), random.randint(1,3))
+        df2 = pd.DataFrame([list(a.pos)], columns=list("XYZ"))
+        df = df.append(df2, ignore_index=True)
+
+    print(df)
+
+    ax = plt.gca()
+    df.plot(kind="scatter", x="X", y="Y", ax=ax)
+    plt.show()
+
+    input("Press Enter to finish")
 
 
 if __name__ == "__main__":
--- project1/package1/functions.py
+++ project1/package1/functions.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def f1():
     print(f'This is f1')
 
 def f2():
     print(f'This is f2')
+
+def f3():
+    x = np.linspace(0, 20, 100)
+    plt.plot(x, np.sin(x))
+    plt.show(block=False)
+    input('press <ENTER> to continue')
